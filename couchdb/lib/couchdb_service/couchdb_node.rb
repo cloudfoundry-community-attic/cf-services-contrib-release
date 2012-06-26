cloudfoundry/vcap-services

Add optional version argument to provision  - Add default supported versions to all nodes
Change-Id: Ib5be8db3e8e2fecd4088fdd2fb9dad5bb5254f61

couchdb/lib/couchdb_service/couchdb_node.rb
@@ -74,6 +74,8 @@ class VCAP::Services::CouchDB::Node
     # TODO: Investigate if there is a better way than this
     # Start couchdb process if its not already started
     @logger.info(`#{@couchdb_ctl} start`) if !couchdb_running?
+
+    @supported_versions = ["1.2"]
   end
 
   def couchdb_running?
@@ -101,7 +103,7 @@ class VCAP::Services::CouchDB::Node
     end
   end
 
-  def provision(plan, credential = nil)
+  def provision(plan, version=nil, credential = nil)
     raise CouchDbError.new(CouchDbError::COUCHDB_INVALID_PLAN, plan) unless plan.to_s == @plan
     provisioned_service = ProvisionedService.new
     if credential

elasticsearch/lib/elasticsearch_service/elasticsearch_node.rb
@@ -74,6 +74,7 @@ class VCAP::Services::ElasticSearch::Node
     @free_ports = Set.new
     options[:port_range].each {|port| @free_ports << port}
     @mutex = Mutex.new
+    @supported_versions = ["0.19"]
   end
 
   def pre_send_announcement
@@ -182,7 +183,7 @@ class VCAP::Services::ElasticSearch::Node
     }
   end
 
-  def provision(plan, credentials = nil)
+  def provision(plan, version=nil, credentials = nil)
     provisioned_service = ProvisionedService.new
     if credentials
       provisioned_service.name = credentials["name"]

memcached/lib/memcached_service/memcached_node.rb
@@ -124,6 +124,7 @@ class VCAP::Services::Memcached::Node
     @memcached_memory = @options[:memcached_memory]
     @sasl_enabled = @options[:sasl_enabled] || false
     @run_as_user =  @options[:run_as_user] || ""
+    @supported_versions =["1.4"]
   end
 
   def pre_send_announcement
@@ -154,7 +155,7 @@ class VCAP::Services::Memcached::Node
     end
   end
 
-  def provision(plan, credentials = nil, db_file = nil)
+  def provision(plan, version=nil, credentials = nil)
     instance = ProvisionedService.new
     instance.plan = plan
     if credentials
@@ -183,7 +184,7 @@ class VCAP::Services::Memcached::Node
     end
 
     begin
-      instance.pid = start_instance(instance, db_file)
+      instance.pid = start_instance(instance)
       @sasl_admin.create_user(instance.user, instance.password) if @sasl_enabled
       save_instance(instance)
       @logger.debug("Started process #{instance.pid}")
@@ -305,7 +306,7 @@ class VCAP::Services::Memcached::Node
     return str
   end
 
-  def start_instance(instance, db_file = nil)
+  def start_instance(instance)
     @logger.debug("Starting: #{instance.inspect}")
 
     opt = {}

mongodb/lib/mongodb_service/mongodb_node.rb
@@ -108,6 +108,7 @@ class VCAP::Services::MongoDB::Node
     @free_ports = Set.new
     options[:port_range].each {|port| @free_ports << port}
     @mutex = Mutex.new
+    @supported_versions = ["1.8"]
   end
 
   def fetch_port(port=nil)
@@ -203,7 +204,7 @@ class VCAP::Services::MongoDB::Node
     list
   end
 
-  def provision(plan, credential = nil)
+  def provision(plan, version=nil, credential = nil)
     @logger.info("Provision request: plan=#{plan}")
     raise ServiceError.new(MongoDBError::MONGODB_INVALID_PLAN, plan) unless plan == @plan
     port = credential && credential['port'] ? fetch_port(credential['port']) : fetch_port

mysql/lib/mysql_service/node.rb
@@ -74,6 +74,7 @@ class VCAP::Services::Mysql::Node
     @connection_wait_timeout = options[:connection_wait_timeout]
     Mysql2::Client.default_timeout = @connection_wait_timeout
     Mysql2::Client.logger = @logger
+    @supported_versions = ["5.1"]
   end
 
   def pre_send_announcement
@@ -247,7 +248,7 @@ class VCAP::Services::Mysql::Node
     @kill_long_transaction_lock.unlock if acquired
   end
 
-  def provision(plan, credential=nil)
+  def provision(plan, version=nil, credential=nil)
     raise MysqlError.new(MysqlError::MYSQL_INVALID_PLAN, plan) unless plan == @plan
     provisioned_service = ProvisionedService.new
     provisioned_service.plan = 1

postgresql/lib/postgresql_service/node.rb
@@ -50,6 +50,7 @@ class VCAP::Services::Postgresql::Node
     @long_tx_killed = 0
     @provision_served = 0
     @binding_served = 0
+    @supported_versions = ["9.0"]
   end
 
   def pre_send_announcement
@@ -313,7 +314,7 @@ class VCAP::Services::Postgresql::Node
     @logger.warn("PostgreSQL error: #{e}")
   end
 
-  def provision(plan, credential=nil)
+  def provision(plan, version=nil, credential=nil)
     raise PostgresqlError.new(PostgresqlError::POSTGRESQL_INVALID_PLAN, plan) unless plan == @plan
     provisionedservice = Provisionedservice.new
     provisionedservice.plan = 1

vblob/lib/vblob_service/vblob_node.rb
@@ -108,6 +108,7 @@ class VCAP::Services::VBlob::Node
     @free_ports = Set.new
     options[:port_range].each {|port| @free_ports << port}
     @mutex = Mutex.new
+    @supported_versions = ["1.0"]
   end
 
   def fetch_port(port=nil)
@@ -208,7 +209,7 @@ class VCAP::Services::VBlob::Node
   end
 
   # will be re-used by restore codes; thus credential could be none null
-  def provision(plan, credential = nil)
+  def provision(plan, version=nil, credential = nil)
     @logger.debug("Provision a service instance")
 
     port = credential && credential['port'] ? fetch_port(credential['port']) : fetch_port

cloudfoundry-community-attic/cf-services-contrib-release

couchdb/lib/couchdb_service/couchdb_node.rb
@@ -74,6 +74,8 @@ class VCAP::Services::CouchDB::Node
     # TODO: Investigate if there is a better way than this
     # Start couchdb process if its not already started
     @logger.info(`#{@couchdb_ctl} start`) if !couchdb_running?
+
+    @supported_versions = ["1.2"]
   end
 
   def couchdb_running?
@@ -101,7 +103,7 @@ class VCAP::Services::CouchDB::Node
     end
   end
 
-  def provision(plan, credential = nil)
+  def provision(plan, credential = nil, version=nil)
     raise CouchDbError.new(CouchDbError::COUCHDB_INVALID_PLAN, plan) unless plan.to_s == @plan
     provisioned_service = ProvisionedService.new
     if credential

elasticsearch/lib/elasticsearch_service/elasticsearch_node.rb
@@ -74,6 +74,7 @@ class VCAP::Services::ElasticSearch::Node
     @free_ports = Set.new
     options[:port_range].each {|port| @free_ports << port}
     @mutex = Mutex.new
+    @supported_versions = ["0.19"]
   end
 
   def pre_send_announcement
@@ -182,7 +183,7 @@ class VCAP::Services::ElasticSearch::Node
     }
   end
 
-  def provision(plan, credentials = nil)
+  def provision(plan, credentials = nil, version=nil)
     provisioned_service = ProvisionedService.new
     if credentials
       provisioned_service.name = credentials["name"]

memcached/lib/memcached_service/memcached_node.rb
@@ -124,6 +124,7 @@ class VCAP::Services::Memcached::Node
     @memcached_memory = @options[:memcached_memory]
     @sasl_enabled = @options[:sasl_enabled] || false
     @run_as_user =  @options[:run_as_user] || ""
+    @supported_versions =["1.4"]
   end
 
   def pre_send_announcement
@@ -154,7 +155,7 @@ class VCAP::Services::Memcached::Node
     end
   end
 
-  def provision(plan, credentials = nil, db_file = nil)
+  def provision(plan, credentials = nil, version=nil)
     instance = ProvisionedService.new
     instance.plan = plan
     if credentials
@@ -183,7 +184,7 @@ class VCAP::Services::Memcached::Node
     end
 
     begin
-      instance.pid = start_instance(instance, db_file)
+      instance.pid = start_instance(instance)
       @sasl_admin.create_user(instance.user, instance.password) if @sasl_enabled
       save_instance(instance)
       @logger.debug("Started process #{instance.pid}")
@@ -305,7 +306,7 @@ class VCAP::Services::Memcached::Node
     return str
   end
 
-  def start_instance(instance, db_file = nil)
+  def start_instance(instance)
     @logger.debug("Starting: #{instance.inspect}")
 
     opt = {}

mongodb/lib/mongodb_service/mongodb_node.rb
@@ -108,6 +108,7 @@ class VCAP::Services::MongoDB::Node
     @free_ports = Set.new
     options[:port_range].each {|port| @free_ports << port}
     @mutex = Mutex.new
+    @supported_versions = ["1.8"]
   end
 
   def fetch_port(port=nil)
@@ -203,7 +204,7 @@ class VCAP::Services::MongoDB::Node
     list
   end
 
-  def provision(plan, credential = nil)
+  def provision(plan, credential = nil, version=nil)
     @logger.info("Provision request: plan=#{plan}")
     raise ServiceError.new(MongoDBError::MONGODB_INVALID_PLAN, plan) unless plan == @plan
     port = credential && credential['port'] ? fetch_port(credential['port']) : fetch_port

mysql/lib/mysql_service/node.rb
@@ -74,6 +74,7 @@ class VCAP::Services::Mysql::Node
     @connection_wait_timeout = options[:connection_wait_timeout]
     Mysql2::Client.default_timeout = @connection_wait_timeout
     Mysql2::Client.logger = @logger
+    @supported_versions = ["5.1"]
   end
 
   def pre_send_announcement
@@ -247,7 +248,7 @@ class VCAP::Services::Mysql::Node
     @kill_long_transaction_lock.unlock if acquired
   end
 
-  def provision(plan, credential=nil)
+  def provision(plan, credential=nil, version=nil)
     raise MysqlError.new(MysqlError::MYSQL_INVALID_PLAN, plan) unless plan == @plan
     provisioned_service = ProvisionedService.new
     provisioned_service.plan = 1

postgresql/lib/postgresql_service/node.rb
@@ -50,6 +50,7 @@ class VCAP::Services::Postgresql::Node
     @long_tx_killed = 0
     @provision_served = 0
     @binding_served = 0
+    @supported_versions = ["9.0"]
   end
 
   def pre_send_announcement
@@ -313,7 +314,7 @@ class VCAP::Services::Postgresql::Node
     @logger.warn("PostgreSQL error: #{e}")
   end
 
-  def provision(plan, credential=nil)
+  def provision(plan, credential=nil, version=nil)
     raise PostgresqlError.new(PostgresqlError::POSTGRESQL_INVALID_PLAN, plan) unless plan == @plan
     provisionedservice = Provisionedservice.new
     provisionedservice.plan = 1

vblob/lib/vblob_service/vblob_node.rb
@@ -108,6 +108,7 @@ class VCAP::Services::VBlob::Node
     @free_ports = Set.new
     options[:port_range].each {|port| @free_ports << port}
     @mutex = Mutex.new
+    @supported_versions = ["1.0"]
   end
 
   def fetch_port(port=nil)
@@ -208,7 +209,7 @@ class VCAP::Services::VBlob::Node
   end
 
   # will be re-used by restore codes; thus credential could be none null
-  def provision(plan, credential = nil)
+  def provision(plan, credential = nil, version=nil)
     @logger.debug("Provision a service instance")
 
     port = credential && credential['port'] ? fetch_port(credential['port']) : fetch_port