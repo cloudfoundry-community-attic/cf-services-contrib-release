cloudfoundry/vcap-services

Add optional version argument to provision  - Add default supported versions to all nodes
Change-Id: Ib5be8db3e8e2fecd4088fdd2fb9dad5bb5254f61

couchdb/lib/couchdb_service/couchdb_node.rb
@@ -74,6 +74,8 @@ class VCAP::Services::CouchDB::Node
     # TODO: Investigate if there is a better way than this
     # Start couchdb process if its not already started
     @logger.info(`#{@couchdb_ctl} start`) if !couchdb_running?
+
+    @supported_versions = ["1.2"]
   end
 
   def couchdb_running?
@@ -101,7 +103,7 @@ class VCAP::Services::CouchDB::Node
     end
   end
 
-  def provision(plan, credential = nil)
+  def provision(plan, version=nil, credential = nil)
     raise CouchDbError.new(CouchDbError::COUCHDB_INVALID_PLAN, plan) unless plan.to_s == @plan
     provisioned_service = ProvisionedService.new
     if credential

memcached/lib/memcached_service/memcached_node.rb
@@ -124,6 +124,7 @@ class VCAP::Services::Memcached::Node
     @memcached_memory = @options[:memcached_memory]
     @sasl_enabled = @options[:sasl_enabled] || false
     @run_as_user =  @options[:run_as_user] || ""
+    @supported_versions =["1.4"]
   end
 
   def pre_send_announcement
@@ -154,7 +155,7 @@ class VCAP::Services::Memcached::Node
     end
   end
 
-  def provision(plan, credentials = nil, db_file = nil)
+  def provision(plan, version=nil, credentials = nil)
     instance = ProvisionedService.new
     instance.plan = plan
     if credentials
@@ -183,7 +184,7 @@ class VCAP::Services::Memcached::Node
     end
 
     begin
-      instance.pid = start_instance(instance, db_file)
+      instance.pid = start_instance(instance)
       @sasl_admin.create_user(instance.user, instance.password) if @sasl_enabled
       save_instance(instance)
       @logger.debug("Started process #{instance.pid}")
@@ -305,7 +306,7 @@ class VCAP::Services::Memcached::Node
     return str
   end
 
-  def start_instance(instance, db_file = nil)
+  def start_instance(instance)
     @logger.debug("Starting: #{instance.inspect}")
 
     opt = {}

cloudfoundry-community-attic/cf-services-contrib-release

 - Update vcap-common and vcap-services-base gem references
 - Add git gem reference for warden-client as required by vcap-services-base
 - Update echo service

couchdb/lib/couchdb_service/couchdb_node.rb
@@ -71,6 +71,8 @@ class VCAP::Services::CouchDB::Node
     # TODO: Investigate if there is a better way than this
     # Start couchdb process if its not already started
     @logger.info(`#{@couchdb_ctl} start`) if !couchdb_running?
+
+    @supported_versions = ["1.2"]
   end
 
   def couchdb_running?
@@ -98,7 +100,7 @@ class VCAP::Services::CouchDB::Node
     end
   end
 
-  def provision(plan, credential = nil)
+  def provision(plan, credential = nil, version=nil)
     raise CouchDbError.new(CouchDbError::COUCHDB_INVALID_PLAN, plan) unless plan.to_s == @plan
     provisioned_service = ProvisionedService.new
     if credential

memcached/lib/memcached_service/memcached_node.rb
@@ -120,6 +120,7 @@ class VCAP::Services::Memcached::Node
     @memcached_memory = @options[:memcached_memory]
     @sasl_enabled = @options[:sasl_enabled] || false
     @run_as_user =  @options[:run_as_user] || ""
+    @supported_versions =["1.4"]
   end
 
   def pre_send_announcement
@@ -150,7 +151,7 @@ class VCAP::Services::Memcached::Node
     end
   end
 
-  def provision(plan, credentials = nil, db_file = nil)
+  def provision(plan, credentials = nil, version=nil)
     instance = ProvisionedService.new
     instance.plan = plan
     if credentials
@@ -179,7 +180,7 @@ class VCAP::Services::Memcached::Node
     end
 
     begin
-      instance.pid = start_instance(instance, db_file)
+      instance.pid = start_instance(instance)
       @sasl_admin.create_user(instance.user, instance.password) if @sasl_enabled
       save_instance(instance)
       @logger.debug("Started process #{instance.pid}")
@@ -301,7 +302,7 @@ class VCAP::Services::Memcached::Node
     return str
   end
 
-  def start_instance(instance, db_file = nil)
+  def start_instance(instance)
     @logger.debug("Starting: #{instance.inspect}")
 
     opt = {}