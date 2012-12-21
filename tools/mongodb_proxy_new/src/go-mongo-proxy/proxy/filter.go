cloudfoundry/vcap-services

[services-ng] commit new mongodb proxy in a big patch
Change-Id: Iac67273c91c97eed3355e0a06cf4d4e504e74478

tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/filter.go
@@ -1,12 +1,17 @@
 package proxy
 
 import (
+	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 )
 
 const BLOCKED = 1
 const UNBLOCKED = 0
+
+const DIRTY_EVENT = 'd'
+const STOP_EVENT = 's'
 
 type FilterConfig struct {
 	BASE_DIR        string // mongo data base dir
@@ -25,38 +30,227 @@ type ConnectionInfo struct {
 
 type Filter interface {
 	FilterEnabled() bool
-	PassFilter() bool
-	StorageMonitor()
+	PassFilter(op_code int) bool
+	IsDirtyEvent(op_code int) bool
+	EnqueueDirtyEvent()
+	StartStorageMonitor()
+	WaitForFinish()
 }
 
 type ProxyFilterImpl struct {
 	// atomic value, use atomic wrapper function to operate on it
-	blocked uint32 // 0 means not block, 1 means block
+	mablocked uint32 // 0 means not block, 1 means block
+	mfblocked uint32 // 0 means not block, 1 means block
+
+	// event channel
+	evtchn1 chan byte // DIRTY event, STOP event
+	evtchn2 chan byte // DIRTY event
 
 	config *FilterConfig
 	mongo  *ConnectionInfo
+
+	// goroutine wait channel
+	lock    sync.Mutex
+	running uint32
+	wait    chan byte
 }
 
 func NewFilter(conf *FilterConfig, conn *ConnectionInfo) *ProxyFilterImpl {
 	return &ProxyFilterImpl{
-		blocked: UNBLOCKED,
-		config:  conf,
-		mongo:   conn}
+		mablocked: UNBLOCKED,
+		mfblocked: UNBLOCKED,
+		evtchn1:   make(chan byte, 100),
+		evtchn2:   make(chan byte, 1),
+		config:    conf,
+		mongo:     conn,
+		running:   0,
+		wait:      make(chan byte, 1)}
 }
 
 func (filter *ProxyFilterImpl) FilterEnabled() bool {
 	return filter.config.ENABLED
 }
 
-func (filter *ProxyFilterImpl) PassFilter() bool {
-	// FIXME: fake filter handler
-	return true
+// If data size exceeds quota or disk files number exceeds quota,
+// then we block the client operations.
+func (filter *ProxyFilterImpl) PassFilter(op_code int) bool {
+	// When we read state of 'mfblockeded', the state of 'mablocked' may
+	// change from 'UNBLOCKED' to 'BLOCKED', so, our implementation only
+	// achieves soft limit not hard limit. Since we have over quota storage
+	// space settings, this is not a big issue.
+	return (op_code != OP_UPDATE && op_code != OP_INSERT) ||
+		(atomic.LoadUint32(&filter.mablocked) == UNBLOCKED &&
+			atomic.LoadUint32(&filter.mfblocked) == UNBLOCKED)
 }
 
-func (filter *ProxyFilterImpl) StorageMonitor() {
-	for {
-		// FIXME: fake monitor handler
-		atomic.StoreUint32(&filter.blocked, UNBLOCKED)
-		time.Sleep(1 * time.Second)
+func (filter *ProxyFilterImpl) IsDirtyEvent(op_code int) bool {
+	return op_code == OP_UPDATE || op_code == OP_INSERT ||
+		op_code == OP_DELETE
+}
+
+func (filter *ProxyFilterImpl) EnqueueDirtyEvent() {
+	filter.evtchn1 <- DIRTY_EVENT
+}
+
+func (filter *ProxyFilterImpl) StartStorageMonitor() {
+	go filter.MonitorQuotaDataSize()
+	go filter.MonitorQuotaFiles()
+}
+
+func (filter *ProxyFilterImpl) WaitForFinish() {
+	if filter.config.ENABLED {
+		filter.evtchn1 <- STOP_EVENT
+		filter.evtchn2 <- STOP_EVENT
+		<-filter.wait
 	}
+}
+
+// Data size monitor depends on output format of mongodb command, the format is
+// united in all of current supported versions, 1.8, 2.0 and 2.2. And we must
+// get the data size information from mongodb command interface.
+func (filter *ProxyFilterImpl) MonitorQuotaDataSize() {
+	dbhost := filter.mongo.HOST
+	port := filter.mongo.PORT
+	dbname := filter.mongo.DBNAME
+	user := filter.mongo.USER
+	pass := filter.mongo.PASS
+	quota_data_size := filter.config.QUOTA_DATA_SIZE
+
+	filter.lock.Lock()
+	filter.running++
+	filter.lock.Unlock()
+
+	var size float64
+	for {
+		event := <-filter.evtchn1
+		if event == STOP_EVENT {
+			break
+		}
+
+		if err := startMongoSession(dbhost, port); err != nil {
+			logger.Error("Failed to connect to %s:%s, [%s].", dbhost, port, err)
+			goto Error
+		}
+
+		if !readMongodbSize(dbname, user, pass, &size) {
+			logger.Error("Failed to read database '%s' size.", dbname)
+			goto Error
+		}
+
+		if size >= float64(quota_data_size)*float64(1024*1024) {
+			logger.Critical("Data size exceeds quota.")
+			atomic.StoreUint32(&filter.mablocked, BLOCKED)
+		} else {
+			atomic.StoreUint32(&filter.mablocked, UNBLOCKED)
+		}
+
+		continue
+	Error:
+		atomic.StoreUint32(&filter.mablocked, BLOCKED)
+	}
+
+	endMongoSession()
+
+	filter.lock.Lock()
+	filter.running--
+	if filter.running == 0 {
+		filter.wait <- STOP_EVENT
+	}
+	filter.lock.Unlock()
+}
+
+// Data file number monitor depends on mongodb disk file layout, the layout is
+// united in all of current supported versions, 1.8, 2.0 and 2.2.
+//
+// For example:
+//
+// Say base dir path is '/tmp/mongodb' and database name is 'db', then the disk
+// file layout would be, /tmp/mongodb/db.ns, /tmp/mongodb/db.0, /tmp/mongodb/db.1,
+// and /tmp/mongodb/db.2 ...
+func (filter *ProxyFilterImpl) MonitorQuotaFiles() {
+	var fd, wd int
+	var err error
+	buffer := make([]byte, 256)
+	dbfiles := make(map[string]int)
+
+	dbname := filter.mongo.DBNAME
+	base_dir := filter.config.BASE_DIR
+	quota_files := filter.config.QUOTA_FILES
+
+	filter.lock.Lock()
+	filter.running++
+	filter.lock.Unlock()
+
+	filecount := 0
+	filecount = iterateDatafile(dbname, base_dir, dbfiles)
+	if filecount < 0 {
+		logger.Error("Failed to iterate data files under %s.", base_dir)
+		goto Error
+	}
+
+	logger.Info("At the begining time we have disk files: [%d].", filecount)
+	if filecount > int(quota_files) {
+		logger.Critical("Disk files exceeds quota.")
+		atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+	}
+
+	// Golang does not recommend to invoke system call directly, but
+	// it does not contain any 'inotify' wrapper function
+	fd, err = syscall.InotifyInit()
+	if err != nil {
+		logger.Error("Failed to call InotifyInit: [%s].", err)
+		goto Error
+	}
+
+	wd, err = syscall.InotifyAddWatch(fd, base_dir, syscall.IN_CREATE|syscall.IN_OPEN|
+		syscall.IN_MOVED_TO|syscall.IN_DELETE)
+	if err != nil {
+		logger.Error("Failed to call InotifyAddWatch: [%s].", err)
+		syscall.Close(fd)
+		goto Error
+	}
+
+	for {
+		select {
+		case event := <-filter.evtchn2:
+			if event == STOP_EVENT {
+				break
+			}
+		default:
+		}
+
+		_, err := asyncRead(syscall.Read, fd, buffer, time.Second)
+		if err != nil {
+			if err == ErrTimeout {
+				continue
+			}
+			logger.Error("Failed to read inotify event: [%s].", err)
+			break
+		} else {
+			filecount = iterateDatafile(dbname, base_dir, dbfiles)
+			if filecount < 0 {
+				logger.Error("Failed to iterate data files under %s.", base_dir)
+				atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+			} else if filecount > int(quota_files) {
+				logger.Critical("Disk files exceeds quota.")
+				atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+			} else {
+				logger.Debug("Current db disk file number: [%d].", filecount)
+				atomic.StoreUint32(&filter.mfblocked, UNBLOCKED)
+			}
+		}
+	}
+
+	syscall.InotifyRmWatch(fd, uint32(wd))
+	syscall.Close(fd)
+
+Error:
+	atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+
+	filter.lock.Lock()
+	filter.running--
+	if filter.running == 0 {
+		filter.wait <- STOP_EVENT
+	}
+	filter.lock.Unlock()
 }

tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/mongoaccess.go
@@ -1,0 +1,116 @@
+package proxy
+
+import (
+	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"strconv"
+)
+
+// Only one caller is allowed to invoked following functions method since we
+// have a global variable here.
+var session *mgo.Session
+
+// singleton object instance
+func startMongoSession(dbhost, port string) error {
+	var err error
+	newsession := false
+
+	if session == nil {
+		newsession = true
+	} else {
+		if err := session.Ping(); err != nil {
+			session.Close()
+			newsession = true
+		}
+	}
+
+	if newsession {
+		session, err = mgo.Dial(dbhost + ":" + port)
+		if err != nil {
+			logger.Error("Failed to connect %s:%s, [%s].", dbhost, port, err)
+			return err
+		}
+	}
+	return nil
+}
+
+func endMongoSession() {
+	if session != nil {
+		session.Close()
+		session = nil
+	}
+}
+
+// Should call 'startMongoSession' before this method.
+//
+// This function depends on output format of 'stats' command, it is united in
+// all of current supported versions, 1.8, 2.0 and 2.2.
+//
+// Output example:
+// {
+//    "db" : "db",
+//    "collections" : 17,
+//    "objects" : 96,
+//    "avgObjSize" : 2621535.5416666665,
+//    "dataSize" : 251667412,
+//    "storageSize" : 305500160,
+//    "numExtents" : 20,
+//    "indexes" : 15,
+//    "indexSize" : 122640,
+//    "fileSize" : 419430400,
+//    "nsSizeMB" : 16,
+//    "ok" : 1
+// }
+func readMongodbSize(dbname, user, pass string, size *float64) bool {
+	var stats bson.M
+	var temp float64
+
+	*size = 0.0
+
+	db := session.DB(dbname)
+	err := db.Login(user, pass)
+	if err != nil {
+		logger.Error("Failed to login database db as %s:%s, [%s].", user, pass, err)
+		return false
+	}
+
+	err = db.Run(bson.D{{"dbStats", 1}, {"scale", 1}}, &stats)
+	if err != nil {
+		logger.Error("Failed to get database %s stats [%s].", dbname, err)
+		return false
+	}
+
+	if !parseMongodbStats(stats["dataSize"], &temp) {
+		logger.Error("Failed to read db_data_size.")
+		return false
+	}
+	db_data_size := temp
+	*size += db_data_size
+
+	if !parseMongodbStats(stats["indexSize"], &temp) {
+		logger.Error("Failed to read db_index_size.")
+		return false
+	}
+	db_index_size := temp
+	*size += db_index_size
+
+	logger.Debug("Get db data total size %v.", *size)
+	return true
+}
+
+/*
+ * NOTE: if disk data file gets very large, then the returned data size value would
+ *       be encoded in 'float' format but not 'integer' format, such as
+ *       2.098026476e+09, if we parse the value in 'integer' format then we get
+ *       error. It always works if we parse an 'integer' value in 'float' format.
+ */
+func parseMongodbStats(value interface{}, result *float64) bool {
+	temp, err := strconv.ParseFloat(fmt.Sprintf("%v", value), 64)
+	if err != nil {
+		logger.Error("Failed to convert data type: [%v].", err)
+		return false
+	}
+	*result = temp
+	return true
+}

cloudfoundry-community-attic/cf-services-contrib-release

tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/filter.go
@@ -1,12 +1,17 @@
 package proxy
 
 import (
+	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 )
 
 const BLOCKED = 1
 const UNBLOCKED = 0
+
+const DIRTY_EVENT = 'd'
+const STOP_EVENT = 's'
 
 type FilterConfig struct {
 	BASE_DIR        string // mongo data base dir
@@ -25,38 +30,231 @@ type ConnectionInfo struct {
 
 type Filter interface {
 	FilterEnabled() bool
-	PassFilter() bool
-	StorageMonitor()
+	PassFilter(op_code int) bool
+	IsDirtyEvent(op_code int) bool
+	EnqueueDirtyEvent()
+	StartStorageMonitor()
+	WaitForFinish()
 }
 
 type ProxyFilterImpl struct {
 	// atomic value, use atomic wrapper function to operate on it
-	blocked uint32 // 0 means not block, 1 means block
+	mablocked uint32 // 0 means not block, 1 means block
+	mfblocked uint32 // 0 means not block, 1 means block
+
+	// event channel
+	data_size_channel  chan byte // DIRTY event, STOP event
+	file_count_channel chan byte // DIRTY event
 
 	config *FilterConfig
 	mongo  *ConnectionInfo
+
+	// goroutine wait channel
+	lock    sync.Mutex
+	running uint32
+	wait    chan byte
 }
 
 func NewFilter(conf *FilterConfig, conn *ConnectionInfo) *ProxyFilterImpl {
 	return &ProxyFilterImpl{
-		blocked: UNBLOCKED,
-		config:  conf,
-		mongo:   conn}
+		mablocked:          UNBLOCKED,
+		mfblocked:          UNBLOCKED,
+		data_size_channel:  make(chan byte, 100),
+		file_count_channel: make(chan byte, 1),
+		config:             conf,
+		mongo:              conn,
+		running:            0,
+		wait:               make(chan byte, 1)}
 }
 
 func (filter *ProxyFilterImpl) FilterEnabled() bool {
 	return filter.config.ENABLED
 }
 
-func (filter *ProxyFilterImpl) PassFilter() bool {
-	// FIXME: fake filter handler
-	return true
+// If data size exceeds quota or disk files number exceeds quota,
+// then we block the client operations.
+func (filter *ProxyFilterImpl) PassFilter(op_code int) bool {
+	// When we read state of 'mfblockeded', the state of 'mablocked' may
+	// change from 'UNBLOCKED' to 'BLOCKED', so, our implementation only
+	// achieves soft limit not hard limit. Since we have over quota storage
+	// space settings, this is not a big issue.
+	return (op_code != OP_UPDATE && op_code != OP_INSERT) ||
+		(atomic.LoadUint32(&filter.mablocked) == UNBLOCKED &&
+			atomic.LoadUint32(&filter.mfblocked) == UNBLOCKED)
 }
 
-func (filter *ProxyFilterImpl) StorageMonitor() {
-	for {
-		// FIXME: fake monitor handler
-		atomic.StoreUint32(&filter.blocked, UNBLOCKED)
-		time.Sleep(1 * time.Second)
+func (filter *ProxyFilterImpl) IsDirtyEvent(op_code int) bool {
+	return op_code == OP_UPDATE || op_code == OP_INSERT ||
+		op_code == OP_DELETE
+}
+
+func (filter *ProxyFilterImpl) EnqueueDirtyEvent() {
+	filter.data_size_channel <- DIRTY_EVENT
+}
+
+func (filter *ProxyFilterImpl) StartStorageMonitor() {
+	go filter.MonitorQuotaDataSize()
+	go filter.MonitorQuotaFiles()
+}
+
+func (filter *ProxyFilterImpl) WaitForFinish() {
+	if filter.config.ENABLED {
+		filter.data_size_channel <- STOP_EVENT
+		filter.file_count_channel <- STOP_EVENT
+		<-filter.wait
 	}
+}
+
+// Data size monitor depends on output format of mongodb command, the format is
+// united in all of current supported versions, 1.8, 2.0 and 2.2. And we must
+// get the data size information from mongodb command interface.
+func (filter *ProxyFilterImpl) MonitorQuotaDataSize() {
+	dbhost := filter.mongo.HOST
+	port := filter.mongo.PORT
+	dbname := filter.mongo.DBNAME
+	user := filter.mongo.USER
+	pass := filter.mongo.PASS
+	quota_data_size := filter.config.QUOTA_DATA_SIZE
+
+	filter.lock.Lock()
+	filter.running++
+	filter.lock.Unlock()
+
+	var size float64
+	for {
+		event := <-filter.data_size_channel
+		if event == STOP_EVENT {
+			break
+		}
+
+		if err := startMongoSession(dbhost, port); err != nil {
+			logger.Error("Failed to connect to %s:%s, [%s].", dbhost, port, err)
+			goto Error
+		}
+
+		if !readMongodbSize(dbname, user, pass, &size) {
+			logger.Error("Failed to read database '%s' size.", dbname)
+			goto Error
+		}
+
+		if size >= float64(quota_data_size)*float64(1024*1024) {
+			logger.Critical("Data size exceeds quota.")
+			atomic.StoreUint32(&filter.mablocked, BLOCKED)
+		} else {
+			atomic.StoreUint32(&filter.mablocked, UNBLOCKED)
+		}
+
+		continue
+	Error:
+		atomic.StoreUint32(&filter.mablocked, BLOCKED)
+	}
+
+	endMongoSession()
+
+	filter.lock.Lock()
+	filter.running--
+	if filter.running == 0 {
+		filter.wait <- STOP_EVENT
+	}
+	filter.lock.Unlock()
+}
+
+// Data file number monitor depends on mongodb disk file layout, the layout is
+// united in all of current supported versions, 1.8, 2.0 and 2.2.
+//
+// For example:
+//
+// Say base dir path is '/tmp/mongodb' and database name is 'db', then the disk
+// file layout would be, /tmp/mongodb/db.ns, /tmp/mongodb/db.0, /tmp/mongodb/db.1,
+// and /tmp/mongodb/db.2 ...
+func (filter *ProxyFilterImpl) MonitorQuotaFiles() {
+	var fd, wd, nread int
+	var err error
+	buffer := make([]byte, 256)
+	dbfiles := make(map[string]int)
+	asyncops := NewAsyncOps()
+
+	dbname := filter.mongo.DBNAME
+	base_dir := filter.config.BASE_DIR
+	quota_files := filter.config.QUOTA_FILES
+
+	filter.lock.Lock()
+	filter.running++
+	filter.lock.Unlock()
+
+	filecount := 0
+	filecount = iterateDatafile(dbname, base_dir, dbfiles)
+	if filecount < 0 {
+		logger.Error("Failed to iterate data files under %s.", base_dir)
+		goto Error
+	}
+
+	logger.Info("At the begining time we have disk files: [%d].", filecount)
+	if filecount > int(quota_files) {
+		logger.Critical("Disk files exceeds quota.")
+		atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+	}
+
+	// Golang does not recommend to invoke system call directly, but
+	// it does not contain any 'inotify' wrapper function
+	fd, err = syscall.InotifyInit()
+	if err != nil {
+		logger.Error("Failed to call InotifyInit: [%s].", err)
+		goto Error
+	}
+
+	wd, err = syscall.InotifyAddWatch(fd, base_dir, syscall.IN_CREATE|syscall.IN_MOVED_TO|syscall.IN_DELETE)
+	if err != nil {
+		logger.Error("Failed to call InotifyAddWatch: [%s].", err)
+		syscall.Close(fd)
+		goto Error
+	}
+
+	defer func() {
+		syscall.InotifyRmWatch(fd, uint32(wd))
+		syscall.Close(fd)
+	}()
+
+	for {
+		select {
+		case event := <-filter.file_count_channel:
+			if event == STOP_EVENT {
+				goto Error
+			}
+		default:
+		}
+
+		nread, err = asyncops.AsyncRead(syscall.Read, fd, buffer, time.Second)
+		if err != nil {
+			if err == ErrTimeout {
+				continue
+			}
+			logger.Error("Failed to read inotify event: [%s].", err)
+			break
+		} else {
+			err = parseInotifyEvent(dbname, buffer[0:nread], &filecount, dbfiles)
+			if err != nil {
+				logger.Error("Failed to parse inotify event.")
+				atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+			} else {
+				logger.Debug("Current db disk file number: [%d].", filecount)
+				if filecount > int(quota_files) {
+					logger.Critical("Disk files exceeds quota.")
+					atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+				} else {
+					atomic.StoreUint32(&filter.mfblocked, UNBLOCKED)
+				}
+			}
+		}
+	}
+
+Error:
+	atomic.StoreUint32(&filter.mfblocked, BLOCKED)
+
+	filter.lock.Lock()
+	filter.running--
+	if filter.running == 0 {
+		filter.wait <- STOP_EVENT
+	}
+	filter.lock.Unlock()
 }

tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/mongoaccess.go
@@ -1,0 +1,114 @@
+package proxy
+
+import (
+	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"strconv"
+)
+
+// Only one caller is allowed to invoked following functions method since we
+// have a global variable here.
+var session *mgo.Session
+
+// singleton object instance
+func startMongoSession(dbhost, port string) error {
+	var err error
+	newsession := false
+
+	if session == nil {
+		newsession = true
+	} else {
+		if err := session.Ping(); err != nil {
+			session.Close()
+			newsession = true
+		}
+	}
+
+	if newsession {
+		session, err = mgo.Dial(dbhost + ":" + port)
+		if err != nil {
+			logger.Error("Failed to connect %s:%s, [%s].", dbhost, port, err)
+			return err
+		}
+	}
+	return nil
+}
+
+func endMongoSession() {
+	if session != nil {
+		session.Close()
+		session = nil
+	}
+}
+
+// Should call 'startMongoSession' before this method.
+//
+// This function depends on output format of 'stats' command, it is united in
+// all of current supported versions, 1.8, 2.0 and 2.2.
+//
+// Output example:
+// {
+//    "db" : "db",
+//    "collections" : 17,
+//    "objects" : 96,
+//    "avgObjSize" : 2621535.5416666665,
+//    "dataSize" : 251667412,
+//    "storageSize" : 305500160,
+//    "numExtents" : 20,
+//    "indexes" : 15,
+//    "indexSize" : 122640,
+//    "fileSize" : 419430400,
+//    "nsSizeMB" : 16,
+//    "ok" : 1
+// }
+func readMongodbSize(dbname, user, pass string, size *float64) bool {
+	var stats bson.M
+	var temp float64
+
+	*size = 0.0
+
+	db := session.DB(dbname)
+	err := db.Login(user, pass)
+	if err != nil {
+		logger.Error("Failed to login database db as %s:%s, [%s].", user, pass, err)
+		return false
+	}
+
+	err = db.Run(bson.D{{"dbStats", 1}, {"scale", 1}}, &stats)
+	if err != nil {
+		logger.Error("Failed to get database %s stats [%s].", dbname, err)
+		return false
+	}
+
+	if !parseMongodbStats(stats["dataSize"], &temp) {
+		logger.Error("Failed to read db_data_size.")
+		return false
+	}
+	*size += temp
+
+	if !parseMongodbStats(stats["indexSize"], &temp) {
+		logger.Error("Failed to read db_index_size.")
+		return false
+	}
+	*size += temp
+
+	logger.Debug("Get db data total size %v.", *size)
+	return true
+}
+
+/*
+ * NOTE: if disk data file gets very large, then the returned data size value would
+ *       be encoded in 'float' format but not 'integer' format, such as
+ *       2.098026476e+09, if we parse the value in 'integer' format then we get
+ *       error. It always works if we parse an 'integer' value in 'float' format.
+ */
+func parseMongodbStats(value interface{}, result *float64) bool {
+	temp, err := strconv.ParseFloat(fmt.Sprintf("%v", value), 64)
+	if err != nil {
+		logger.Error("Failed to convert data type: [%v].", err)
+		return false
+	}
+	*result = temp
+	return true
+}