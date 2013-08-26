24277a6463322f354e1c56c9c339cf196db46ed3
Ruben Koster <ruben.koster@innovationfactory.eu>
2013-08-26 20:38:41 +0200

    Initial terracotta service - move repo into dir

---
+++ src/services/terracotta/lib/terracotta_service/common.rb
@@ -0,0 +1,12 @@
+# Copyright (c) 2009-2011 VMware, Inc.
+module VCAP
+  module Services
+    module Terracotta
+      module Common
+        def service_name
+          "TerracottaaaS"
+        end
+      end
+    end
+  end
+end
