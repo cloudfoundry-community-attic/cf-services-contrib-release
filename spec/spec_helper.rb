61df50243a6333f9086b55eb76caf0942caae77f
Ruben Koster <rkoster@starkandwayne.com>
2014-04-16 10:13:56 +0200

    Added cf helper method for better errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,15 @@ require "common/exec"
 require "excon"
 
 module ContribServices
-  include Bosh::Exec
-
   def deploy_app(app_dir)
     app_path = File.join(File.dirname(__FILE__), "assets", app_dir)
-    sh "cf push #{app_name} -p=#{app_path} --no-start"
+    cf "push #{app_name} -p=#{app_path} --no-start"
   end
 
   def login
     puts "Running tests on #{target} on behalf of #{test_user}"
-    sh "cf api #{target}"
-    sh "cf auth #{test_user} #{test_pwd}"
+    cf "api #{target}"
+    cf "auth #{test_user} #{test_pwd}"
   end
 
   def test_user
@@ -48,7 +46,7 @@ module ContribServices
   end
 
   def system_services
-    @system_services ||= sh("cf marketplace").output
+    @system_services ||= cf("marketplace").output
   end
 
   def service_available?(name)
@@ -57,24 +55,24 @@ module ContribServices
 
   def target_space
     create_space
-    sh "cf target -o #{organization} -s #{space}"
+    cf "target -o #{organization} -s #{space}"
   end
 
   def create_space
-    if sh("cf space #{space}", on_error: :return).failed?
-      sh("cf create-space #{space}")
+    if cf("space #{space}", on_error: :return).failed?
+      cf("create-space #{space}")
     end
   end
 
   def delete_space
-    unless sh("cf space #{space}", on_error: :return).failed?
-      sh "cf delete-space #{space} -f"
+    unless cf("space #{space}", on_error: :return).failed?
+      cf "delete-space #{space} -f"
     end
   end
 
   def start_app
     puts "Starting application (please wait)"
-    sh "cf start #{app_name}"
+    cf "start #{app_name}"
   end
 
   def provision_service(service_type)
@@ -86,22 +84,30 @@ module ContribServices
   end
 
   def create_service(service_type, service_name)
-    sh "cf create-service #{service_type} default #{service_name}"
+    cf "create-service #{service_type} default #{service_name}"
   end
 
   def delete_service(service_name)
-    unless sh("cf service #{service_name}", on_error: :return).failed?
+    unless cf("service #{service_name}", on_error: :return).failed?
       unbind_service(app_name, service_name)
-      sh "cf delete-service #{service_name} -f"
+      cf "delete-service #{service_name} -f"
     end
   end
 
   def bind_service(service_name)
-    sh "cf bind-service #{app_name} #{service_name}"
+    cf "bind-service #{app_name} #{service_name}"
   end
 
   def unbind_service(app_name, service_name)
-    sh "cf unbind-service #{app_name} #{service_name}", on_error: :return
+    cf "unbind-service #{app_name} #{service_name}", on_error: :return
+  end
+
+  def cf(args, options={})
+    begin
+      Bosh::Exec.sh "cf #{args}", options
+    rescue Bosh::Exec::Error => e
+      raise [e.message, e.output].join(":\n")
+    end
   end
 end
 
